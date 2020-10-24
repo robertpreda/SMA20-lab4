b6712e436ce8dbac7efcc4b54b044a67baeb098f
Robert Preda <predarobert7@gmail.com>
2020-10-24 16:20:47 +0300

    confusion

--- Services/app/src/main/java/com/example/services/WebsearchActivity.java
+++ Services/app/src/main/java/com/example/services/WebsearchActivity.java
@@ -2,50 +2,40 @@ package com.example.services;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.EditText;
 
-import static androidx.core.content.ContextCompat.startActivity;
-
-public class MainActivity extends AppCompatActivity {
-
+public class WebsearchActivity extends AppCompatActivity {
     private WebView webView;
-    private EditText urlSearch;
-    private Button searchButton;
-
+    private Button backgroundButton, foregroundButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_websearch);
+
+        this.backgroundButton = findViewById(R.id.background);
+        this.foregroundButton = findViewById(R.id.foreground);
 
         this.webView = findViewById(R.id.webView);
-        this.urlSearch = findViewById(R.id.url);
-        this.searchButton = findViewById(R.id.search);
         WebSettings settings = this.webView.getSettings();
         settings.setJavaScriptEnabled(true);
 
         webView.setWebViewClient(new CustomWebViewClient());
 
-//        this.webView.loadUrl("to ad");
 
     }
-
     public void click(View view){
         switch (view.getId()){
-            case R.id.search:
-                String url = this.urlSearch.getText().toString();
-                this.webView.loadUrl(url);
+            case R.id.background:
                 break;
             case R.id.foreground:
-                Intent i = new Intent(this, ImageActivity.class);
-
+                break;
+            default:
+                break;
         }
     }
 }
+
